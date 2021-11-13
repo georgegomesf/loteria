c9fd99a48521ff63aef271b9b69e3112fbdae894
George Gomes <hiperlogado@gmail.com>
2021-11-13 00:36:58 -0300

    Seleção de data.

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from 'axios'
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+
+  const { dia } = req.body;
+
+  const response = await axios.get(`https://servico.loteriafort.site/api/Resultado/${process.env.NEXT_PUBLIC_LOTERIA_KEY}/0/${dia}/${dia}`)
+  
+  res.status(200).json(response.data)
+  
 }
--- pages/loteria.js
+++ pages/loteria.js
@@ -67,6 +67,40 @@ const useStyles = makeStyles((theme) => ({
         minWidth: '2.5rem',
         fontWeight: '700',
         fontSize: '1.3rem'
+    },
+    filtro: {
+        padding: "0.4rem 0.4rem",
+        minWidth: '4rem'
+      },
+    select: {
+        cursor: 'pointer',
+        minWidth: '10rem',        
+        color: "#fff",
+        padding: "1rem 0 1.5rem",
+        margin: "0.6rem 0 1rem",
+        fontSize: "1.1rem",
+        height: "2rem",
+        textAlign: "center",
+        borderColor: '#fff',
+        backgroundColor: "rgba(0, 0, 0,0.1)",
+        '&:hover': {
+            backgroundColor: "rgba(0,0,0,0.2)",
+        },
+        '&:focus': {
+            backgroundColor: "rgba(0,0,0,0.3)",
+        },
+        '&:before': {
+            borderColor: '#orange'
+        },
+        '&:after': {
+            borderColor: 'orange',
+        },        
+    },
+    filtros: {
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignItems: 'center'
     }
 }));
 
@@ -78,9 +112,10 @@ export default function Admin() {
     const [loading,setLoading] = useState(false);
     const [access,setAccess] = useState(false);
     const [atualiza,setAtualiza] = useState(false)
-    const [atualizacao,setAtualizacao] = useState()
     const [atual,setAtual] = useState()
     const [jogos,setJogos] = useState({})
+    const [extracao,setExtracao] = useState();
+    const [ultimas,setUltimas] = useState();
 
     useEffect(() => {
         const loginCheck = () => {
@@ -107,55 +142,71 @@ export default function Admin() {
             
             const atualizacao = collection(db,'atualizacao')
             const docAtual = await getDoc(doc(atualizacao,"dados"))
-            setAtual(format(parseISO(docAtual.data().datahora),"dd/MM/Y à's' hh:mm'h'"))
-            setLoading(false)
-            console.table(docs.docs.map(i=>i.data()))
-            
-        }
-        pegaJogos()
-    }, [,atualiza]) 
- 
+            setAtual(docAtual.data().extracao)
 
-    const atualizar = async () => { 
+            const collUltima = collection(db,'extracoes')
+            const docUltima = await getDocs(query(collUltima,orderBy("id","desc"),limit(21)))
+            setUltimas(docUltima.docs.map(i=>i.data()))            
+            
+            setLoading(false)
+             
+        }        
+        pegaJogos()
+        
+    }, [,atualiza])     
+
+    useEffect(() => {
+
+        const pegaNovos = async () => {
+            setLoading(true)
+            const hoje = new Date()
+            const listDate = [];
+            const startDate = format(hoje - (40 * 24 * 60 * 60 * 1000),"Y-MM-dd");
+            const endDate = format(hoje,"Y-MM-dd");
+
+            const dateMove = new Date(startDate);
+            let strDate = startDate;
+
+            const coll = collection(db,'extracoes');
+            const docs = await getDocs(coll);
+        
+            while (strDate < endDate){
+                strDate = dateMove.toISOString().slice(0, 10);
+                listDate.push(strDate);
+                const diaMove = dateMove.setDate(dateMove.getDate() + 1);
+                const dia = format(diaMove,"Y-MM-dd");            
+                const docsDia = await getDocs(query(coll,where('data','==',dia)));                        
+                if(docsDia.docs.length==0 || docs.docs[docs.docs.length-1].data().data==dia){
+                    const ext = await axios.post('/api/hello', { dia: dia })
+                    ext.data.map(i=> {  
+                        const diaId = format(diaMove,"YMMdd")           
+                        setDoc(doc(coll,diaId+i.nome_loteria.replace('LOTERIA FORT ','').replace(':','')),
+                        {             
+                            id: parseInt(diaId+i.nome_loteria.replace('LOTERIA FORT ','').replace(':','')),
+                            data: i.data_resultado.substring(6,10) + '-' + i.data_resultado.substring(3,5) + '-' + i.data_resultado.substring(0,2),
+                            hora: i.nome_loteria.replace('LOTERIA FORT ','')+':00',
+                            premios: 
+                            Array.from({length: 10}, (x, j) => j).map(p=> {
+                                return i.premios[p].valor.substring(3,5)=="00" ? 25 : Math.ceil(parseInt(i.premios[p].valor.substring(3,5))/4)
+                            })
+                        })
+                    })
+                    console.log(dia)
+                }   
+            };
+            setLoading(false)
+        }
+        pegaNovos()
+    }, [])  
+
+    const atualizar = async (event) => { 
 
         setLoading(true)
         const hoje = new Date()
-        const listDate = [];
-        const startDate = format(hoje - (40 * 24 * 60 * 60 * 1000),"Y-MM-dd");
-        const endDate = format(hoje,"Y-MM-dd");
-
-        const dateMove = new Date(startDate);
-        let strDate = startDate;
-
-        const coll = collection(db,'extracoes');
-        const docs = await getDocs(coll);
-    
-        while (strDate < endDate){
-            strDate = dateMove.toISOString().slice(0, 10);
-            listDate.push(strDate);
-            const diaMove = dateMove.setDate(dateMove.getDate() + 1);
-            const dia = format(diaMove,"Y-MM-dd");            
-            const docsDia = await getDocs(query(coll,where('data','==',dia)));                        
-            if(docsDia.docs.length==0 || docs.docs[docs.docs.length-1].data().data==dia){
-                const ext = await axios.get(`https://servico.loteriafort.site/api/Resultado/422428E7-A88F-44EB-9E14-D1DC8E3E2B40/0/${dia}/${dia}`)
-                ext.data.map(i=> {  
-                    const diaId = format(diaMove,"YMMdd")           
-                    setDoc(doc(coll,diaId+i.nome_loteria.replace('LOTERIA FORT ','').replace(':','')),
-                    {             
-                        id: parseInt(diaId+i.nome_loteria.replace('LOTERIA FORT ','').replace(':','')),
-                        data: i.data_resultado.substring(6,10) + '-' + i.data_resultado.substring(3,5) + '-' + i.data_resultado.substring(0,2),
-                        hora: i.nome_loteria.replace('LOTERIA FORT ','')+':00',
-                        premios: 
-                        Array.from({length: 10}, (x, j) => j).map(p=> {
-                            return i.premios[p].valor.substring(3,5)=="00" ? 25 : Math.ceil(parseInt(i.premios[p].valor.substring(3,5))/4)
-                        })
-                    })
-                })
-                console.log(dia)
-            }   
-        };
         
-        const docsCal = await getDocs(query(coll,orderBy("data","desc"),limit(220)))
+        const coll = collection(db,'extracoes');
+        
+        const docsCal = await getDocs(query(coll,where('id','<=',event.target.value),orderBy("id","desc"),limit(220)))
         const docsExt = Array.from({length: 10}, (x, i) => i).map(p=> {
             return Array.from({length: 25}, (y, j) => j).map(n=> {                                
                 return docsCal.docs.filter(f=>f.data().premios[p]==n+1).map(n=>{ return parseInt(n.data().data.replaceAll('-','')) } ).reduce(function(a, b) {
@@ -168,7 +219,7 @@ export default function Admin() {
                     }
                 })
             })
-        })                
+        })
 
         const collCalculos = collection(db,'calculos');
         docsExt.map(j => {
@@ -183,7 +234,8 @@ export default function Admin() {
                         dias: intervalToDuration({
                             start: parseISO(i.ultimoDia.toString().substring(0,4) + '-' + i.ultimoDia.toString().substring(4,6) + '-' + i.ultimoDia.toString().substring(6,8) + ' 00:00:00'),
                             end: hoje
-                        }).days 
+                        }).days,
+                        extracoes: docsCal.docs.filter(f=>f.id>i.ultimaExtracao).length
                     }
                 })                
                 docPremios.length>0 && setDoc(
@@ -198,9 +250,9 @@ export default function Admin() {
         const atualizacao = collection(db,'atualizacao')
         const docAtual = await getDoc(doc(atualizacao,"dados"))
         if(docAtual.data()){            
-            updateDoc(doc(atualizacao,"dados"),{ datahora: format(hoje,"yyyy-MM-dd'T'HH:mm:ss.SSSxxx") })
+            updateDoc(doc(atualizacao,"dados"),{ extracao: event.target.value })
         } else {
-            setDoc(doc(atualizacao,"dados"),{ datahora: format(hoje,"yyyy-MM-dd'T'HH:mm:ss.SSSxxx") })
+            setDoc(doc(atualizacao,"dados"),{ extracao: event.target.value })
         }
 
         setLoading(false)
@@ -208,10 +260,10 @@ export default function Admin() {
     }
 
     const ordenar = ( b,a ) => {
-        if ( a.dias < b.dias ){
+        if ( a.extracoes < b.extracoes ){
           return -1;
         }
-        if ( a.dias > b.dias ){
+        if ( a.extracoes > b.extracoes ){
           return 1;
         }
         return 0;
@@ -231,11 +283,25 @@ export default function Admin() {
           color: "black" 
         }} />
         <main className={styles.main}>
-        {user && access && <div className={classes.root}>
-            <div>            
-            Última Atualização em {atual}
-            <Button onClick={()=>atualizar()} className={classes.botao}>Atualizar</Button>
-            </div>
+        {user && access && ultimas && <div className={classes.root}>
+            <div className={classes.filtros}>            
+                <div>Exibindo dados até: &nbsp;</div>
+                <div>                
+                <FormControl variant="filled" sx={{ m: 1, minWidth: 120 }}>
+                    <InputLabel id="demo-simple-select-label" style={{color:'#fff', padding: '0.2rem'}}></InputLabel>
+                    <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={atual ? atual : ultimas[0].id}
+                        onChange={atualizar}
+                        className={classes.select}
+                        defaultValue={atual ? atual.toString().substring(6,8) + '/' + atual.toString().substring(4,6) + '/' + atual.toString().substring(0,4) + ' ' + atual.toString().substring(8,10) + ':' + atual.toString().substring(10,12) : format(parseISO(ultimas[0].data+' '+ultimas[0].hora),"dd/MM/Y '-' HH:mm'h")}
+                        >
+                        {ultimas.map(i=> <MenuItem value={i.id}>{format(parseISO(i.data+' '+i.hora),"dd/MM/Y ' - ' HH:mm'h")}</MenuItem>)}
+                    </Select>
+                </FormControl>
+                </div>
+            </div>            
             <Paper className={classes.paper}>
             {Array.from({length: 10}, (x, j) => j).map((p,indexp) => <Card className={classes.card} key={indexp}>
                 <div className={classes.titulo}>{p+1}º Premio</div>
@@ -244,8 +310,8 @@ export default function Admin() {
                 i.grupos.sort(ordenar).map((h,index)=>
                     index<=8 && <div>
                         <Chip className={classes.chip} variant='outlined' label={`${h.grupo}`} />
-                        <span className={classes.subtitulo}>{h.dias} dias</span>
-                        <div className={classes.subtitulo}>Última vez: {
+                        <span className={classes.subtitulo}>{h.extracoes} extrações</span>
+                        <div className={classes.subtitulo}>Última: {
                             h.ultimaExtracao.toString().substring(6,8) + '/' + h.ultimaExtracao.toString().substring(4,6) + '/' + h.ultimaExtracao.toString().substring(0,4) + ' ' + h.ultimaExtracao.toString().substring(8,10) + ':' + h.ultimaExtracao.toString().substring(10,12)
                         }</div>
                     </div>
